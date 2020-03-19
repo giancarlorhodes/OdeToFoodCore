22b2408cb0824ccd56dc21860d9565a9bbc9c767
giancarlorhodes <giancarlo.rhodes@gmail.com>
2020-03-19 14:17:30 -0500

    entity framework and db creation steps

---
+++ OdeToFoodCoreData/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFoodCoreCommon;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFoodCoreData
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
